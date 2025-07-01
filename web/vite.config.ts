5cc8f1f654f4b70cd354a561cb86be827c3f890e
PhyschicWinter9 <suratlhaodee@gmail.com>
2025-07-02 00:12:45 +0700

    refactor : github workflow

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(),tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/thai-adblock-list/' : '/',
+   base: command === 'build' ? '/thai-adblock-list/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
     minify: true
   }
-})
+}))
