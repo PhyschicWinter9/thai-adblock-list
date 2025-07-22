cd46e0a887dc19b7b0738516e916a924e266a255
PhyschicWinter9 <suratlhaodee@gmail.com>
2025-07-22 13:54:41 +0700

    fix : change pipeline gh page

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react(),tailwindcss()],
-   base: command === 'build' ? '/thai-adblock-list/' : '/',
+export default defineConfig(() => ({
+  plugins: [react(), tailwindcss()],
+  base: "/thai-adblock-list/",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     sourcemap: false,
-    minify: true
-  }
-}))
+    minify: true,
+  },
+}));
